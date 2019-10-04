a9c5fb9494d6d6c675ca0f89867677247c80ea2b
Name <author@test.com>
2019-10-04 13:34:30 +0300

    111

--- src/main/java/pack_2/Class_1.java
+++ src/main/java/pack_2/Class_1.java
@@ -3,14 +3,13 @@ package pack_2;
 public class Class_1 {
 
     public static void bar1() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println(Class_1.class.getName());
         }
     }
 
     public static void main(String[] args) {
         bar1();
+        //another
     }
-
-
 }
